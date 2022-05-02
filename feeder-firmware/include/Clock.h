24b748086d8378c94776e7289884b32a597e7127
pedro leao <pedroforestileao@gmail.com>
2022-05-02 13:43:20 -0300

    Created clock class and its methods

--- feeder-firmware/include/Clock.h
+++ feeder-firmware/include/Clock.h
@@ -1,21 +1,28 @@
 #pragma once
-#include <time.h>
+#include <chrono>
+#include <Arduino.h>
+using namespace std::chrono;
 
 class Clock {
 private:
     static Clock* instance;
-    time_t currentTime;
-    void makeRequest();
     Clock();
+    steady_clock::time_point last;
+
+    int hours;
+    int minutes;
+    int seconds;
 
 public:
     static Clock* getInstance();
     ~Clock();
 
-    int getHour();
-    int getMinutes();
-    int getSeconds();
+    int getHour() const;
+    int getMinutes() const;
+    int getSeconds() const;
     void update();
+
+    String toString() const;    
 };
 
 Clock* Clock::instance = nullptr;
--- feeder-firmware/src/Clock.cpp
+++ feeder-firmware/src/Clock.cpp
@@ -1,6 +1,11 @@
 #include "Clock.h"
 
 Clock::Clock() {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    last = steady_clock::now();
 }
 
 Clock::~Clock() {
@@ -12,4 +17,50 @@ Clock* Clock::getInstance() {
         instance = new Clock();
 
     return instance;
+}
+
+int Clock::getHour() const
+{
+    return hours;
+}
+
+int Clock::getMinutes() const
+{
+    return minutes;
+}
+
+int Clock::getSeconds() const
+{
+    return seconds;
+}
+
+void Clock::update()
+{
+    steady_clock::time_point now = steady_clock::now();
+
+    seconds += duration_cast<std::chrono::seconds>(now - last).count();
+
+    last = now;
+
+    if (seconds >= 60)
+    {
+        minutes++;
+        seconds = 0;
+    }
+
+    if (minutes >= 60)
+    {
+        hours++;
+        minutes = 0;
+    }
+
+    if (hours >= 24)
+    {
+        hours = 0;
+    }
+}
+
+String Clock::toString() const
+{
+    return String(hours) + ":" + String(minutes) + ":" + String(seconds);
 }
